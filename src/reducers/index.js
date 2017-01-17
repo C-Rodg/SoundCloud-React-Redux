6af185d0fdbb3a82fb035f78be3814706ef1af62
Kobaine <rodgersbc@gmail.com>
2017-01-16 16:09:51 -0800

    Setup routing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+
+import { routerReducer } from 'react-router-redux';
+
 import track from './track';
 
 export default combineReducers({
-    track
+    track,
+    routing : routerReducer
 });
